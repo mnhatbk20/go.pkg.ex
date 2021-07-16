6063d7236fd990195fcab579e124728b59946fc4
mnhatbk20 <mnhatbk20@gmail.com>
2021-07-16 17:40:51 +0700

    first commit

---
+++ entry.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"app/mathpkg"
+
+)
+
+func main() {
+	fmt.Println(mathpkg.Add(2, 4))
+}
---
+++ mathpkg/mathfile.go
@@ -0,0 +1,5 @@
+package mathpkg
+
+func Add(a, b int) int {
+	return a + b
+}
